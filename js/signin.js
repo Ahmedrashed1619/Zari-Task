7201d1170dd488c4a9e824f7a921ad8bacc4ac80
ahmedrashed1619 <ar9353062@gmail.com>
2022-07-11 14:47:44 +0200

    register validation

--- js/signin.js
+++ js/signin.js
@@ -17,21 +17,25 @@ let login = document.getElementById('enter-page')
 login.addEventListener('click', loginPage);
 
 function loginPage(){
+
     let passwordSignin = document.getElementById('password-signin');
     let textSignin = document.getElementById('text-signin');
     let text = textSignin.value;
     let pass = passwordSignin.value;
-    let alertSignin = document.getElementById('alertSignin')
+    let alertSignin = document.getElementById('alertSignin');
+
     if(text == '' || pass == ''){
         alertSignin.innerHTML = 'All Fields are important..';
         alertSignin.classList.add('text-danger');
         alertSignin.classList.remove('text-success');
     }
+
     if(JSON.parse(localStorage.getItem('ZariUsers')) == null && text != '' && pass != ''){
         alertSignin.innerHTML = 'There is an error in Email, please make a registration first..';
         alertSignin.classList.add('text-danger');
         alertSignin.classList.remove('text-success');
     }
+    
     else{
         for(let i = 0; i < users.length; i++){
             if(text == users[i].email || text == users[i].phone && pass == users[i].password ){
