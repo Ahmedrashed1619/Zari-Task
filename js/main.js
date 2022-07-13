e87c6e953c135faf1ac67da0f3882458a8e73fa2
ahmedrashed1619 <ar9353062@gmail.com>
2022-07-13 06:04:36 +0200

    add features , how it work and sample sections with responsive

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@
 // })
 
 
+
+// when click event on topbtn & toggle 
+
+// $('#topBtn').click(function(){
+//     $('html , body').animate({scrollTop : 0}, 500);
+// })
+
+$('.navbar .navbar-toggler').click(()=>{
+    $('.navbar .navbar-toggler').toggleClass('convert');
+})
+
+
+// change flag for currency when click 
+
 let currency = document.getElementById('currency');
 let img = document.getElementById('img-select')
 currency.addEventListener('click' , function(){
